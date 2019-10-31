82d58e48306917f65cf0b00f4c937a56be470c3c
Karl Matti <karlmatti@hotmail.com>
2019-10-31 17:43:52 +0200

    Update ldr.py

--- ldr.py
+++ ldr.py
@@ -1,22 +1,20 @@
 import RPi.GPIO as GPIO
 from gpiozero import MCP3008
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(21, GPIO.OUT)
 pwm = GPIO.PWM(21, 100)
 
-print("\nPress Ctrl+C to quit \n")
-dc = 0
-pwm.start(dc)
+print("\nVajuta Ctrl+C et väljuda \n")
+pwm.start(0)
 divider = MCP3008(0)
-
 try:
     while True:
         print(int(divider.value * 100))
         pwm.ChangeDutyCycle(int(divider.value * 100))
+        sleep(0.2)
 except KeyboardInterrupt:
-    print("Ctrl+C pressed - ending program")
-
+    print("Ctrl+C vajutatud - väljun programmist")
 pwm.stop()
 GPIO.cleanup()
